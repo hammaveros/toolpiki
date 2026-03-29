d5c1b7cfc8df9b40c6b5cb32fb037429958334e5
hammaveros <hammaveros@gmail.com>
2026-03-29 18:36:47 +0900

    fix - 채팅방 광고 임시 제거

--- src/app/(ko)/chat/page.tsx
+++ src/app/(ko)/chat/page.tsx
@@ -1,24 +1,12 @@
 'use client';
 
 import { ChatRoom } from '@/components/chat/ChatRoom';
-import { AdSlot } from '@/components/ads/AdSlot';
 
 export default function ChatPage() {
   return (
-    <div className="flex justify-center gap-4 px-4 h-[calc(100vh-64px)]">
-      {/* 왼쪽 광고 - PC만 */}
-      <div className="hidden xl:flex flex-col items-center pt-4 w-[160px] flex-shrink-0">
-        <AdSlot format="vertical" className="sticky top-20" />
-      </div>
-
-      {/* 채팅방 */}
+    <div className="flex justify-center h-[calc(100vh-64px)]">
       <div className="w-full max-w-2xl">
         <ChatRoom />
-      </div>
-
-      {/* 오른쪽 광고 - PC만 */}
-      <div className="hidden xl:flex flex-col items-center pt-4 w-[160px] flex-shrink-0">
-        <AdSlot format="vertical" className="sticky top-20" />
       </div>
     </div>
   );
